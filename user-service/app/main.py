fbbbd46f1bab8490280770967c83888a9c8cba1d
Tanvir Ahmed <tanvirewu32@gmail.com>
2024-11-04 22:53:08 +0900

    Update main.py in user service

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,14 +1,22 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+# Fetch the allowed origins from an environment variable, with a default fallback
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to your frontend URL
+    allow_origins=allowed_origins,  # Use the environment variable here
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],  # Allows all headers
 )
+
+
 @app.get("/")
 def read_root():
     return {"message": "User Service is up and running!"}
