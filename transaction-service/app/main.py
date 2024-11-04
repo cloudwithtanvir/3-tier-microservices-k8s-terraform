b4efbf7facaa780b2cfd2f631693384fafd61e7f
Tanvir Ahmed <tanvirewu32@gmail.com>
2024-11-04 22:53:08 +0900

    Update main.py in transaction service

--- transaction-service/app/main.py
+++ transaction-service/app/main.py
@@ -1,15 +1,23 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from routes import router  # Ensure you have your router set up in routes.py
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+# Fetch the allowed origins from an environment variable, with a default fallback
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to your frontend URL
+    allow_origins=allowed_origins,  # Use the environment variable here
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],  # Allows all headers
 )
+
 # Include your router for other API routes
 app.include_router(router, prefix="/api")
 
